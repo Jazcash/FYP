97c210825b513ef2b28762507f76629d631b0f94
Jazcash <jaspercashmore@gmail.com>
2015-04-29 09:04:35 +0100

    Backup

--- src/FYP - Line Maze/lineMaze.cpp
+++ src/FYP - Line Maze/lineMaze.cpp
@@ -495,34 +495,18 @@ int8_t followSegment(){
 	unsigned int sensors[5];
 	
 	while(1){
-		// Normally, we will be following a line.  The code below is
-		// similar to the 3pi-linefollower-pid example, but the maximum
-		// speed is turned down to 60 for reliability.
-		
-		// Get the position of the line.
 		unsigned int position = read_line(sensors,IR_EMITTERS_ON);
 		
-		// The "proportional" term should be 0 when we are on the line.
 		int proportional = ((int)position) - 1900;
 		
-		// Compute the derivative (change) and integral (sum) of the
-		// position.
 		int derivative = proportional - last_proportional;
 		integral += proportional;
 		
-		// Remember the last position.
 		last_proportional = proportional;
 		
-		// Compute the difference between the two motor power settings,
-		// m1 - m2.  If this is a positive number the robot will turn
-		// to the left.  If it is a negative number, the robot will
-		// turn to the right, and the magnitude of the number determines
-		// the sharpness of the turn.
 		int power_difference = proportional/20 + integral/10000 + derivative*3/2;
-		
-		// Compute the actual motor settings.  We never set either motor
-		// to a negative value.
-		const int max = speed; // the maximum speed
+
+		const int max = speed
 		if(power_difference > max) {
 			power_difference = max;
 		}
@@ -536,11 +520,6 @@ int8_t followSegment(){
 			set_motors(max,max-power_difference);
 		}
 		
-		// We use the inner three sensors (1, 2, and 3) for
-		// determining whether there is a line straight ahead, and the
-		// sensors 0 and 4 for detecting lines going to the left and
-		// right.
-
 		if(sensors[1] < threshhold && sensors[2] < threshhold && sensors[3] < threshhold){
 			// There is no line visible ahead, and we didn't see any
 			// intersection.  Must be a dead end.
@@ -556,14 +535,19 @@ int8_t followSegment(){
 			
 			break;
 		} else if(sensors[0] > threshhold || sensors[4] > threshhold) {
-			// Found an intersection.
+			// Found an intersection
+			
+			// slow down and position sensors fully over the line ahead
 			set_motors(40, 40);
 			delay_ms(50);
 			
+			// take a second reading at the new location and determine the presence of connections
 			read_line(sensors,IR_EMITTERS_ON);
 			isLeft = (sensors[0] > 500);
 			isRight = (sensors[4] > 500);
 
+			// to detect a forward connection, drive the Micromouse forward slightly to avoid incorrect assumption
+			//  that a corner is a forward connection
 			set_motors(40, 40);
 			delay_ms(200);
 			stop();
